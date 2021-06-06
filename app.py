ac43bbc76a3a780dfd6a1f1f6722d9e284622886
arjunraghurama <arjunr0019@gmail.com>
2021-06-06 09:39:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,20 +104,6 @@ progressbar = st.progress(0.0)
 data = get_transaction_df(transactions_per_page,start_idx,progressbar)
 st.table(data)
 
-if not hasattr(st, 'already_started_server'):
-    # Hack the fact that Python modules (like st) only load once to
-    # keep track of whether this file already ran.
-    st.already_started_server = True
-
-    from flask import Flask
-
-    app = Flask(__name__)
-
-    @app.route('/stats')
-    def stats():
-        return str(10)
-
-    app.run(port=8888)
 
 footer="""<style>
 a:link , a:visited{
