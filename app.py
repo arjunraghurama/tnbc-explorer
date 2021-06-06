c7e2aa41605d521aed0cf4e7ac0396ecb17f2479
arjunraghurama <arjunr0019@gmail.com>
2021-06-06 08:57:17 +0530

    Removed commented code

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ def balance():
 
 balance()
 
-slot = st.empty()
 
 #  Transactions
 st.markdown("<h3 style='text-align: left; color: green;'>Latest Transactions</h3>", unsafe_allow_html=True)
@@ -109,14 +108,6 @@ if not hasattr(st, 'already_started_server'):
     # Hack the fact that Python modules (like st) only load once to
     # keep track of whether this file already ran.
     st.already_started_server = True
-
-    st.write('''
-        The first time this script executes it will run forever because it's
-        running a Flask server.
-
-        Just close this browser tab and open a new one to see your Streamlit
-        app.
-    ''')
 
     from flask import Flask
 
