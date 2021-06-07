542bcb91fc2321992809625bd0817360a7123dd8
arjunraghurama <arjunr0019@gmail.com>
2021-06-07 23:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ def balance():
             headers = {}
             response = requests.request("GET", url, headers=headers, data=payload)
             balance = json.loads(response.text)['balance']
-            balance = "{:,}".format(balance)
+            if balance is not None:
+                balance = "{:,}".format(balance)
             st.markdown("<h3 style='text-align: center; color: green;'>Account balance is : {} TNBC </h3>".format(balance), unsafe_allow_html=True)
 
 balance()
